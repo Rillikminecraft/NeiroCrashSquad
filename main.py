414e96525434a74570aeb5542a8400e798782920
Rillikminecraft <151042075+Rillikminecraft@users.noreply.github.com>
2024-02-02 17:04:11 +0300

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('default.html')
+    return render_template('index.html')
 
 @app.route('/download')
 def download():
